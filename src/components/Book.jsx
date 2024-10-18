946ae8039a42f5f702ab667fcefb7b79163001b4
Noccis <toniwincent83@gmail.com>
2024-10-18 09:58:09 +0200

    Created a simple router

---
+++ src/components/Book.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+        <h2>Title</h2>
+        <p>Description</p>
+    </div>
+  )
+}
+
+export default Book
---
+++ src/pages/BookPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookPage = () => {
+  return (
+    <div>
+        <p>Here is where the book library will be.</p>
+    </div>
+  )
+}
+
+export default BookPage
